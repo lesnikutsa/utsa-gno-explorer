6f7635b01a4f36bf14828e663ca8676677f765b1
lesnikutsa <35654095+lesnikutsa@users.noreply.github.com>
2026-07-18 23:42:21 +0300

    Wire network mascot asset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { ExplorerLayout } from './layouts/ExplorerLayout'
 import { Overview } from './pages/Overview'
 import { useExplorerData } from './hooks/useExplorerData'
 
+const NETWORK_MASCOT_SRC = '/assets/network-mascot.png?v=1'
+
 export default function App() {
   const explorerData = useExplorerData()
 
   return (
     <ExplorerLayout healthState={explorerData.healthState} nextFastRefreshAt={explorerData.nextFastRefreshAt}>
-      <Overview explorerData={explorerData} />
+      <Overview
+        explorerData={explorerData}
+        mascotSrc={NETWORK_MASCOT_SRC}
+      />
     </ExplorerLayout>
   )
 }
