3099948f9d632fdac1fb0082fa3387ea5484d6df
lesnikutsa <35654095+lesnikutsa@users.noreply.github.com>
2026-09-05 21:20:18 +0300

    Test validator detail panel order

---
+++ frontend/test/cosmosValidatorPanelOrder.test.js
@@ -0,0 +1,12 @@
+import assert from 'node:assert/strict'
+import fs from 'node:fs'
+import test from 'node:test'
+
+const css = fs.readFileSync(new URL('../src/styles/cosmos-validator-reward-usd.css', import.meta.url), 'utf8')
+
+test('validator detail paired panels use the requested visual order', () => {
+  assert.match(css, /cosmos-validator-detail__secondary > \.cosmos-validator-fields:not\(\.cosmos-validator-reward-fields\)\s*\{[^}]*order:\s*1/)
+  assert.match(css, /cosmos-validator-detail__secondary > \.cosmos-validator-reward-fields\s*\{[^}]*order:\s*2/)
+  assert.match(css, /cosmos-validator-detail__lower > \.cosmos-validator-activity\s*\{[^}]*order:\s*1/)
+  assert.match(css, /cosmos-validator-detail__lower > \.cosmos-validator-delegators\s*\{[^}]*order:\s*2/)
+})
